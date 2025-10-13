fc7ebe378d9606b8aa3c4bee98b6820eda0bcb72
Mohamed Attallah <100219531+mhmdatallaa@users.noreply.github.com>
2025-10-13 20:25:39 +0300

    Persist app data Using CoreData framework(#1)

--- Expensio/Model/Expense.swift
+++ Expensio/Model/Expense.swift
@@ -7,16 +7,16 @@
 
 import Foundation
 
-struct Expense: Identifiable {
-    let id = UUID()
-    let amount: Double
-    let title: String
-    let category: Category
-    let note: String
-    var dateCreated : String {
-        let date = Date()
-        let formater = DateFormatter()
-        formater.dateFormat = "HH:mm"
-        return formater.string(from: date)
-    }
-}
+//struct Expense: Identifiable {
+//    let id = UUID().uuidString
+//    let amount: Double
+//    let title: String
+//    let category: Category
+//    let note: String
+//    var dateCreated : String {
+//        let date = Date()
+//        let formater = DateFormatter()
+//        formater.dateFormat = "HH:mm"
+//        return formater.string(from: date)
+//    }
+//}
